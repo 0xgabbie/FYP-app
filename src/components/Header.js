f3fb75f155bc28ae8dbc31088e831c837562080f
0xgabbie <133480435+0xgabbie@users.noreply.github.com>
2023-05-13 16:24:18 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,6 @@ function Header() {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="/Learning">Learning</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
